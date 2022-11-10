d6f0fc105ffd6cc9aeee43dcfafb016f2a8d6514
MyUsername <alberto.baglivi@gmail.com>
2022-11-10 15:57:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+
+
+export default{
+
+components: {
+  AppHeader,
+
+},
+
+data(){
+
+  return{
+
+
+  }
+}, 
+
+created(){
+
+
+
+}, 
+
+}
+
+</script>
+
+<template>
+
+<AppHeader />
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as*;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    name: "AppHeader"
+}
+
+</script>
+
+<template>
+   <h1>Ciao</h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+    color:red
+}
+</style>
